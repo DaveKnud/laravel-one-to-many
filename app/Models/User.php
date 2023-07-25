0c00de0c69c584a352d882cb2e82bef3ff3df6e6
DaveKnud <daveknud@outlook.com>
2023-07-25 02:13:18 +0200

    Relazioni create e cardinalita' espresse

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function Address()
+    {
+        return $this->hasOne(Address::class);
+    }
+
+    public function Emails()
+    {
+        return $this->hasMany(Email::class);
+    }
 }
